22340053575b9be27232627e74797373ddcb9882
André Almeida <aalalmeida13@gmail.com>
2022-12-29 22:52:44 +0000

    updated

--- providers.tf
+++ providers.tf
@@ -10,6 +10,10 @@ terraform {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.10.0"
+    }
   }
 }
 
