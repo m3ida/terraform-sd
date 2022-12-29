5f1616230978b63a05c740db9378bb6727016ada
André Almeida <aalalmeida13@gmail.com>
2022-12-29 22:57:43 +0000

    fix

--- main.tf
+++ main.tf
@@ -65,6 +65,13 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   }
 }
 
+data "kubectl_file_documents" "namespace" {
+    content = file("${path.cwd}/manifests/argocd/namespace.yml")
+} 
+data "kubectl_file_documents" "argocd" {
+    content = file("${path.cwd}/manifests/argocd/install.yml")
+}
+
 resource "kubectl_manifest" "namespace" {
     count     = length(data.kubectl_file_documents.namespace.documents)
     yaml_body = element(data.kubectl_file_documents.namespace.documents, count.index)
